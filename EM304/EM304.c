37fdd8412241fe2c3a29a619a6d9a633251c1ec7
Fares Mohammed <faresmohammed09.02@gmail.com>
2025-01-26 13:52:44 +0000

    Readded vscode folder

--- EM304/EM304.c
+++ EM304/EM304.c
@@ -67,7 +67,7 @@ int main() {
     while (true) {
         printf("Hello, world! \n wifi + i2c + alarm + interpolator + timer + watchdog\n");
         cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 0);  
-        sleep_ms(500);
+        sleep_ms(250);
 
         cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 1);  
         sleep_ms(500);
